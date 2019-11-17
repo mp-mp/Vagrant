81a0bca668ed009022a3e6509cf3c8ee957a9aa2
mp-mp <mp@l8l.pl>
2019-11-17 17:11:07 +0100

    first commit

---
+++ 1VM/bootstrap.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y aptitude
+aptitude install -y tcpdump
+#aptitude install -y apache2
+#aptitude install -y bridge-utils
+aptitude install -y vlan
+aptitude install -y tmux
+#aptitude install -y python3-pip
+#pip3 install django
+modprobe 8021q
