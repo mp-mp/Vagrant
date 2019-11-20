3883d23e28361a886e71c8ac593b1e2007e832a7
Mateusz Krawczyk <krawat10@gmail.com>
2019-11-20 09:16:12 +0100

    added django server

--- DEBIAN_SWITH/bootstrap.sh
+++ DEBIAN_SWITH/bootstrap.sh
@@ -3,10 +3,12 @@
 apt-get update
 apt-get install -y aptitude
 aptitude install -y tcpdump
-aptitude install -y apache2
 aptitude install -y bridge-utils
 aptitude install -y vlan
 aptitude install -y tmux
 aptitude install -y python3-pip
-pip3 install django
+aptitude install -y git
+git clone https://github.com/krawat10/EthernetSwitch.git
+pip3 install -r EthernetSwitch/requirements.txt
+sudo python3 EthernetSwitch/manage.py runserver 0:80
 modprobe 8021q
